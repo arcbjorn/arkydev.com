02edb6a2a4d311a24bd7a1e6a1734e5f07f0335b
arcbjorn <arky.happ@gmail.com>
2022-09-25 03:38:43 +0400

    feat(components): add LanguageSelect

---
+++ src/components/LanguageSelect.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { lang, langs } from '$l18n';
+</script>
+
+<div class="select-wrapper">
+  <select bind:value={$lang}>
+    {#each langs as lang}
+      <option value={lang}>{lang}</option>
+    {/each}
+  </select>
+</div>
+
+<style>
+  .select-wrapper {
+    @apply px-4 sm:px-8;
+  }
+</style>
