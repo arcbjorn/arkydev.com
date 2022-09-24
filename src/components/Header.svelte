82a514285fa30e61b0a91af552a1b171b01c46d2
arcbjorn <arky.happ@gmail.com>
2022-09-25 03:27:00 +0400

    feat(components): add Navigation in Header

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,51 +1,14 @@
 <script lang="ts">
-    import type { INavigationOption } from '$routes/types';
-	import { Token } from "$i18n/enums";
-
-    const navOptions: INavigationOption[] = [
-      {
-        name: Token.HOME,
-        anchor: '/#'
-      },
-      {
-        name: Token.SERVICES,
-        anchor: `#${Token.SERVICES}`
-      },
-      {
-        name: Token.WHY,
-        anchor: `#${Token.WHY}`
-      },
-      {
-        name: Token.ABOUT,
-        anchor: `#${Token.ABOUT}`
-      },
-      {
-        name: Token.CONTACT,
-        anchor: `#${Token.CONTACT}`
-      }
-    ]
+  import Navigation from '$components/Navigation.svelte';
 </script>
 
 <header>
-  logo
-  <nav>
-    {#each navOptions as option}
-    <a href={option.anchor} class="option">
-      {option.name}
-    </a>
-    {/each}
-  </nav>
+  <a href="/#">logo</a>
+  <Navigation />
 </header>
 
 <style>
   header {
     @apply sticky top-0 z-30 w-full py-8 sm:py-4 flex justify-around;
   }
-  nav {
-    @apply flex justify-around invisible lg:visible;
-  }
-  .option {
-    @apply px-4 sm:px-8;
-  }
-	
 </style>
---
+++ src/components/Navigation.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { t } from '$l18n';
+
+  import type { INavigationOption } from '$routes/types';
+  import { EToken } from '$l18n/enums';
+
+  const navOptions: INavigationOption[] = [
+    {
+      token: EToken.SERVICES,
+      anchor: `#${EToken.SERVICES}`
+    },
+    {
+      token: EToken.ABOUT,
+      anchor: `#${EToken.ABOUT}`
+    },
+    {
+      token: EToken.CONTACT,
+      anchor: `#${EToken.CONTACT}`
+    }
+  ];
+</script>
+
+<nav>
+  {#each navOptions as option}
+    <a href={option.anchor} class="option">
+      {$t(option.token)}
+    </a>
+  {/each}
+</nav>
+
+<style>
+  nav {
+    @apply flex justify-around invisible lg:visible;
+  }
+  .option {
+    @apply px-4 sm:px-8;
+  }
+</style>
