a692e93ba0c06be1ca669a464ce099393b1d4519
arcbjorn <arky.happ@gmail.com>
2022-09-25 00:36:33 +0400

    ref(components): add Header

---
+++ src/components/Header.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    import type { INavigationOption } from '$routes/types';
+	import { Token } from "$i18n/enums";
+
+    const navOptions: INavigationOption[] = [
+      {
+        name: Token.HOME,
+        anchor: '/#'
+      },
+      {
+        name: Token.SERVICES,
+        anchor: `#${Token.SERVICES}`
+      },
+      {
+        name: Token.WHY,
+        anchor: `#${Token.WHY}`
+      },
+      {
+        name: Token.ABOUT,
+        anchor: `#${Token.ABOUT}`
+      },
+      {
+        name: Token.CONTACT,
+        anchor: `#${Token.CONTACT}`
+      }
+    ]
+</script>
+
+<header>
+  logo
+  <nav>
+    {#each navOptions as option}
+    <a href={option.anchor} class="px-2">
+      {option.name}
+    </a>
+    {/each}
+  </nav>
+</header>
+
+<style>
+  header {
+    @apply sticky top-0 z-30 w-full px-4 py-4 flex justify-around;
+  }
+  nav {
+    @apply flex justify-around;
+  }
+	
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,51 +1,11 @@
 <script lang="ts">
-    import "../app.css";
-    import type { INavigationOption } from '$routes/types';
-	  import { Token } from "$i18n/enums";
-
-    const navOptions: INavigationOption[] = [
-      {
-        name: Token.HOME,
-        anchor: '/#'
-      },
-      {
-        name: Token.SERVICES,
-        anchor: `#${Token.SERVICES}`
-      },
-      {
-        name: Token.WHY,
-        anchor: `#${Token.WHY}`
-      },
-      {
-        name: Token.ABOUT,
-        anchor: `#${Token.ABOUT}`
-      },
-      {
-        name: Token.CONTACT,
-        anchor: `#${Token.CONTACT}`
-      }
-    ]
+  import "../app.css";
+  import Header from "$components/Header.svelte";
 </script>
 
-<header>
-  logo
-  <nav>
-    {#each navOptions as option}
-    <a href={option.anchor} class="px-2">
-      {option.name}
-    </a>
-    {/each}
-  </nav>
-</header>
+<Header />
 
 <slot></slot>
 
 <style>
-  header {
-    @apply sticky top-0 z-30 w-full px-4 py-4 sm:px-4 flex justify-around;
-  }
-  nav {
-    @apply flex justify-around;
-  }
-	
 </style>
