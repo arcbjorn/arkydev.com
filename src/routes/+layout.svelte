7532786280fe1207c4f515f0b698da1c475973ad
arcbjorn <arky.happ@gmail.com>
2022-09-25 05:16:11 +0400

    ref(layout & page): positioning

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,5 @@
 <script lang="ts">
   import '../app.css';
-  import Header from '$components/header/Header.svelte';
 </script>
 
-<div class="flex flex-col h-full">
-  <Header />
-  <slot />
-</div>
-
-<style>
-</style>
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
   import Main from '$components/Main.svelte';
+  import Header from '$components/header/Header.svelte';
 </script>
 
-<Main />
+<div class="flex flex-col h-full">
+  <Header />
+  <Main />
+</div>
