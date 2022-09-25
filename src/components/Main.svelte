272ac90ea88d2998ad93e0979ce5b6c56c122a99
arcbjorn <arky.happ@gmail.com>
2022-09-25 04:12:04 +0400

    feat(components): add Main & base styles

---
+++ src/components/Main.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  //   import Navigation from '$components/header/Navigation.svelte';
+  //   import LanguageSelect from './header/LanguageSelect.svelte';
+</script>
+
+<main>test</main>
+
+<style>
+  main {
+    @apply w-full py-8 flex justify-around border-b-2 bg-white grow;
+  }
+</style>
