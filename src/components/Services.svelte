05d980a90118f49cdc01492df3bda4e76920569e
arcbjorn <arky.happ@gmail.com>
2022-09-25 05:48:27 +0400

    feat(components): add Services

---
+++ src/components/Services.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import { EToken } from '$l18n/enums';
+
+  // import { t } from '$l18n';
+  // import { EToken } from '$l18n/enums';
+</script>
+
+<div id={EToken.SERVICES} class="services">
+  <div>Service1</div>
+  <div>Service2</div>
+  <div>Service3</div>
+  <div>Service4</div>
+</div>
+
+<style>
+  .services {
+    @apply w-full flex flex-col justify-around py-8 border-b-2 bg-white;
+  }
+</style>
