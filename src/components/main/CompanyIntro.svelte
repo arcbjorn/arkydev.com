98ecea6909fde18804d7eed7c0ff99123dbf69ce
arcbjorn <arky.happ@gmail.com>
2022-09-26 01:31:34 +0400

    feat(components/main): add CompanyIntro

---
+++ src/components/main/CompanyIntro.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import { t } from '$l18n';
+  import { EToken } from '$l18n/enums';
+  import { ECompanyName } from '$components/enums';
+  import Logo from '$components/main/Logo.svelte';
+</script>
+
+<div class="flex p-8 border-2 bg-white border-black">
+  <Logo />
+  <div class="flex flex-col pl-4 gap-y-2">
+    <span class="text-2xl sm:text-4xl">{ECompanyName.FULL}</span>
+    <span class="text-md sm:text-xl">{$t(EToken.SLOGAN)}</span>
+  </div>
+</div>
