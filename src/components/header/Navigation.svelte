e499be8c243c2bb8407b58fc02426d6af926126d
arcbjorn <arky.happ@gmail.com>
2022-09-25 05:47:49 +0400

    feat(Navigation): add & use NAVIGATION_ANCHOR

--- src/components/header/Navigation.svelte
+++ src/components/header/Navigation.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
   import { t } from '$l18n';
 
-  import type { INavigationOption } from '$routes/types';
+  import { NAVIGATION_ANCHOR, type INavigationOption } from '$routes/types';
   import { EToken } from '$l18n/enums';
 
   const navOptions: INavigationOption[] = [
     {
       token: EToken.SERVICES,
-      anchor: `#${EToken.SERVICES}`
+      anchor: NAVIGATION_ANCHOR[EToken.SERVICES]
     },
     {
       token: EToken.ABOUT,
-      anchor: `#${EToken.ABOUT}`
+      anchor: NAVIGATION_ANCHOR[EToken.ABOUT]
     },
     {
       token: EToken.CONTACT,
-      anchor: `#${EToken.CONTACT}`
+      anchor: NAVIGATION_ANCHOR[EToken.CONTACT]
     }
   ];
 </script>
--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,8 +1,16 @@
-import type { EToken } from '$l18n/enums';
+import { EToken } from '$l18n/enums';
 
 type TNavigationToken = EToken.SERVICES | EToken.ABOUT | EToken.CONTACT;
 
+type TNavigationAnchor = Record<TNavigationToken, string>;
+
+export const NAVIGATION_ANCHOR: TNavigationAnchor = {
+  [EToken.SERVICES]: `#${EToken.SERVICES}`,
+  [EToken.ABOUT]: `#${EToken.ABOUT}`,
+  [EToken.CONTACT]: `#${EToken.CONTACT}`
+};
+
 export interface INavigationOption {
-	token: TNavigationToken;
-	anchor: string;
+  token: TNavigationToken;
+  anchor: string;
 }
