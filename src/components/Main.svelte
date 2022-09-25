e0f766464fdeb827ed7a474ce9b1fc65aa560304
arcbjorn <arky.happ@gmail.com>
2022-09-25 05:47:55 +0400

    feat(Main): add Call to action button

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { t } from '$l18n';
   import { EToken } from '$l18n/enums';
+  import { NAVIGATION_ANCHOR } from '$routes/types';
 </script>
 
 <main>
@@ -9,8 +10,13 @@
       <div class="text-4xl">Arky Software Development</div>
       <div>{$t(EToken.SLOGAN)}</div>
     </div>
-    <div class="text-xl">
-      {@html $t(EToken.DESCRIPTION)}
+    <div class="flex flex-col">
+      <div class="text-xl pb-4">
+        {@html $t(EToken.DESCRIPTION)}
+      </div>
+      <a href={NAVIGATION_ANCHOR[EToken.SERVICES]}>
+        <button class="call-to-action">{$t(EToken.CALL)}</button>
+      </a>
     </div>
   </div>
 </main>
@@ -18,5 +24,13 @@
 <style>
   main {
     @apply grow w-full flex flex-col justify-around py-8 border-b-2 bg-white;
+  }
+
+  .call-to-action {
+    @apply p-4 border-2 bg-white;
+  }
+
+  .call-to-action:hover {
+    @apply bg-black text-white;
   }
 </style>
