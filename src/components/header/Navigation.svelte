b3e14234b26a25993b7a6b51df0d14d7e8f770b2
arcbjorn <arky.happ@gmail.com>
2022-09-26 01:53:57 +0400

    feat(Navigation): add hover styles

--- src/components/header/Navigation.svelte
+++ src/components/header/Navigation.svelte
@@ -22,8 +22,38 @@
 
 <nav class="flex justify-around">
   {#each navOptions as option}
-    <a href={option.anchor} class="px-4 sm:px-8">
+    <a href={option.anchor} class="option">
       {$t(option.token)}
     </a>
   {/each}
 </nav>
+
+<style>
+  .option {
+    @apply px-4 sm:px-8;
+  }
+
+  .option:hover {
+    position: relative;
+
+    background: linear-gradient(
+          to bottom,
+          black 1.2px,
+          transparent 1.2px,
+          transparent calc(100% - 1.2px),
+          black calc(100% - 1.2px)
+        )
+        no-repeat,
+      linear-gradient(
+          to left,
+          black 1.2px,
+          transparent 1.2px,
+          transparent calc(100% - 1.2px),
+          black calc(100% - 1.2px)
+        )
+        no-repeat;
+
+    background-position: center;
+    background-size: calc(100% - 0.75em) 100%, 100% calc(100% - 0.75em);
+  }
+</style>
