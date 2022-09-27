80a1897d2570c23c78437bd71b51271eaaf385c6
arcbjorn <arky.happ@gmail.com>
2022-09-27 15:06:23 +0400

    Updates

--- app_/immutable/chunks/0-fae28bae.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-98712e5d.js";export{t as component};
---
+++ app_/immutable/chunks/1-1f9e7191.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/error.svelte-77c97835.js";export{t as component};
