9892ac4cdc5691e1aa4aa4ed944191d861f0682b
arcbjorn <arky.happ@gmail.com>
2022-09-28 01:59:33 +0400

    build(SvelteKit): configure SPA mode

--- svelte.config.js
+++ svelte.config.js
@@ -1,22 +1,27 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess({
-		postcss: true
-	}),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess({
+    postcss: true
+  }),
 
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$routes: 'src/routes',
-			$components: 'src/components',
-			$l18n: 'src/l18n'
-		}
-	}
+  kit: {
+    adapter: adapter({
+      fallback: 'index.html'
+    }),
+
+    prerender: { entries: [] },
+
+    alias: {
+      $routes: 'src/routes',
+      $components: 'src/components',
+      $l18n: 'src/l18n'
+    }
+  }
 };
 
 export default config;
