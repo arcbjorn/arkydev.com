f47c4ae79320917a02e834c5e5ec12705c2e7778
arcbjorn <arky.happ@gmail.com>
2022-09-28 02:00:43 +0400

    feat(i18n): add remaining translations

--- src/l18n/enums.ts
+++ src/l18n/enums.ts
@@ -1,13 +1,39 @@
 export enum ELanguage {
-  EN = 'en',
-  DE = 'de'
+  EN = 'en'
 }
 
 export enum EToken {
   SERVICES = 'services',
   ABOUT = 'about',
   CONTACT = 'contact',
-  SLOGAN = 'slogan',
-  DESCRIPTION = 'description',
-  CALL = 'call'
+
+  SLOGAN = 'main.slogan',
+  DESCRIPTION = 'main.description',
+  ACTION = 'main.action',
+
+  CONSULTING = 'services.consulting',
+  CONSULTING_DESC = 'services.consulting.description',
+  CONSULTING_ACTION = 'services.consulting.call',
+
+  ARCHITECTURE = 'services.architecture',
+  ARCHITECTURE_DESC = 'services.architecture.description',
+  ARCHITECTURE_ACTION = 'services.architecture.call',
+
+  TEAM = 'services.team',
+  TEAM_DESC = 'services.team.description',
+  TEAM_ACTION = 'services.team.call',
+
+  COMPLETE = 'services.complete',
+  COMPLETE_DESC = 'services.complete.description',
+  COMPLETE_ACTION = 'services.complete.call',
+
+  ABOUT_US = 'about.description',
+
+  PARTNERS = 'partners',
+
+  CONTACT_ACTION = 'contact.action',
+  CONTACT_LINKS = 'contact.links',
+
+  PRIVACY = 'privacy',
+  TERMS = 'terms'
 }
--- src/l18n/translations.ts
+++ src/l18n/translations.ts
@@ -7,18 +7,41 @@ const translations: TTranslations = {
     [EToken.ABOUT]: 'About us',
     [EToken.CONTACT]: 'Contact',
     [EToken.SLOGAN]: 'Efficiency. Reliability. Consistency.',
+
     [EToken.DESCRIPTION]:
-      'We create state-of-art systems,<br />accurately meeting the demand of business,<br/>constantly bringing positive impact on the world.',
-    [EToken.CALL]: 'Discuss a project'
-  },
-  [ELanguage.DE]: {
-    [EToken.SERVICES]: 'Dienstleistungen',
-    [EToken.ABOUT]: 'Über uns',
-    [EToken.CONTACT]: 'Kontakt',
-    [EToken.SLOGAN]: 'Efficiency. Reliability. Consistency.',
-    [EToken.DESCRIPTION]:
-      'We create state-of-art systems,<br />accurately meeting the demand of business,<br/>constantly bringing positive impact on the world.',
-    [EToken.CALL]: 'Discuss a project'
+      'We create state-of-art software systems to bring positive impact on the world.',
+    [EToken.ACTION]: 'Discuss a project',
+
+    [EToken.CONSULTING]: 'Consulting',
+    [EToken.CONSULTING_DESC]:
+      'Analytical process of current & future systems to find the most efficient & reliable way of achieving business objectives.',
+    [EToken.CONSULTING_ACTION]: 'Let`s chat',
+
+    [EToken.ARCHITECTURE]: 'System Architecture',
+    [EToken.ARCHITECTURE_DESC]:
+      'Restructure of current system or construction of a system from ground up with resiliency and consistency in mind.',
+    [EToken.ARCHITECTURE_ACTION]: 'Start deepwork',
+
+    [EToken.TEAM]: 'Team Integration',
+    [EToken.TEAM_DESC]:
+      'Ready to take on a challenge and join the existing team to improve quality & speed at any stage of development.',
+    [EToken.TEAM_ACTION]: 'Hire us',
+
+    [EToken.COMPLETE]: 'Complete solution',
+    [EToken.COMPLETE_DESC]:
+      '<li>Design & Discovery</li><li>System Architecture</li><li>Implementation</li><li>DevOps</li>',
+    [EToken.COMPLETE_ACTION]: 'Launch big idea',
+
+    [EToken.ABOUT_US]:
+      'We are the team of seasoned engineers & designers, dedicated to make the world a better place with help of technologies:',
+
+    [EToken.PARTNERS]: 'Greatful to have opportunity to work with:',
+
+    [EToken.CONTACT_ACTION]: 'Let`s do it!',
+    [EToken.CONTACT_LINKS]: 'Contact us via',
+
+    [EToken.PRIVACY]: 'Privacy Policy',
+    [EToken.TERMS]: 'Terms of Usage'
   }
 };
 
