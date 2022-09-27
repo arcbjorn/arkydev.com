95efda8674bce01c1fab6d1983a8672b1a812960
arcbjorn <arky.happ@gmail.com>
2022-09-28 02:07:44 +0400

    feat(page):Technologies, Partners, Contact, Footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,10 @@
   import Main from '$components/main/Main.svelte';
   import Services from '$components/services/Services.svelte';
   import Header from '$components/header/Header.svelte';
+  import Contact from '$components/contact/Contact.svelte';
+  import Footer from '$components/footer/Footer.svelte';
+  import Partners from '$components/partners/Partners.svelte';
+  import Technologies from '$components/technologies/Technologies.svelte';
 </script>
 
 <div class="flex flex-col h-full">
@@ -10,3 +14,7 @@
 </div>
 
 <Services />
+<Technologies />
+<Partners />
+<Contact />
+<Footer />
