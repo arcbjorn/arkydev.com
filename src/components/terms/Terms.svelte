431b5b1df1671ae9fad229f89428f06bf917b463
arcbjorn <arky.happ@gmail.com>
2022-09-30 02:17:57 +0400

    feat(terms): add Terms of usage page

---
+++ src/components/terms/Terms.svelte
@@ -0,0 +1,184 @@
+<script>
+  import { ECompanyName } from '$components/enums';
+  import { t } from '$l18n';
+  import { EToken } from '$l18n/enums';
+</script>
+
+<div class="flex justify-center py-8 border-b-2 bg-white">
+  <div class="prose">
+    <h1>{$t(EToken.TERMS)}</h1>
+    <p>
+      <b>
+        {ECompanyName.FULL} Terms of Usage ("Agreement")
+      </b>
+    </p>
+    <p>
+      {ECompanyName.FULL} is Georgia corporation. Please read these Terms of Usage ("Agreement", "Terms
+      of Usage") carefully before registering on any sites (“Sites”) or using any of the web applications
+      ("the Applications") operated by {ECompanyName.ABBREVIATION} ("us", "we", or "our"). This Agreement
+      sets forth the legally binding terms and conditions for your use of the Applications. Definitions
+    </p>
+    <ul>
+      <li>
+        <b> Sites </b>
+        - https://arkydev.com and all subdomains, subdirectories.
+      </li>
+      <li>
+        <b> Applications </b>
+        - Any application developed by {ECompanyName.FULL}.
+      </li>
+      <li>
+        <b> End User </b>
+        - Any user of the "Applications"
+      </li>
+    </ul>
+    <p>
+      By accessing or using the site in any manner, including, but not limited to, visiting or
+      browsing the Site or contributing content or other materials to the Site, you agree to be
+      bound by these Terms of Usage. Capitalized terms are defined in this Agreement.
+    </p>
+
+    <h2>Intellectual Property</h2>
+    <p>
+      Sites and web applications and associated content, features and functionality are owned by {ECompanyName.ABBREVIATION}
+      and are protected by international copyright & trademark.
+    </p>
+    <h2>Termination</h2>
+    <p>
+      We may terminate your use of the Sites and Applications without cause or notice, which may
+      result in the forfeiture and destruction of all information associated with you. All
+      provisions of this Agreement that by their nature should survive termination shall survive
+      termination, including, without limitation, ownership provisions, warranty disclaimers,
+      indemnity, and limitations of liability.
+    </p>
+    <h2>Fees and Services</h2>
+    <p>
+      All references on the Sites and Applications or in this Membership Terms to “$” and “dollars”
+      are referring to United States Dollars (USD).
+    </p>
+    <h2>Ongoing Monthly Membership</h2>
+    <p>
+      If you have chosen a monthly term, your {ECompanyName.ABBREVIATION} membership will continue month-to-month
+      unless and until you cancel your membership or we terminate it. You must cancel your membership
+      before it renews each month in order to avoid billing of the next month's membership fees to your
+      Payment Method. Monthly renewals occur on the "month anniversary" (with the exception of shorter
+      months, whereby your account may be charged up to 3-days before your "month anniversary") of your
+      initial purchase date. We will bill the monthly membership fee to the Payment Method you provide
+      to us during registration (or to a different Payment Method if you change your account information).
+      Membership fees are fully earned upon payment.
+    </p>
+    <h2>Ongoing Yearly Membership</h2>
+    <p>
+      If you have chosen a yearly term, your {ECompanyName.ABBREVIATION} membership will continue year-to-year
+      unless and until you cancel your membership or we terminate it. You must cancel your membership
+      before it renews each year in order to avoid billing of the next year's membership fees to your
+      Payment Method. Yearly renewals occur on the "month and day anniversary" (with the exception of
+      shorter months, whereby your account may be charged up to 3-days before your "month and day anniversary")
+      of your initial purchase date. We will bill the yearly membership fee to the Payment Method you
+      provide to us during registration (or to a different Payment Method if you change your account
+      information). Membership fees are fully earned upon payment.
+    </p>
+    <p>
+      {ECompanyName.ABBREVIATION} (paid) monthly and yearly pricing is subject to change which will be
+      reflected on your account. This pricing will not reflect your account if the price change is due
+      to promotion.
+    </p>
+    <h2>Subscription Credit Card Authorization</h2>
+    <p>
+      By entering into a monthly or yearly subscription with us, you are explicitly authorizing us
+      to charge your credit card on a recurring basis. When upgrading your {ECompanyName.ABBREVIATION}
+      account, you hereby agree that all immediate and future charges as per our Plans are pre-authorized.
+      Pre-authorized charges on monthly and yearly accounts are non-refundable.
+    </p>
+    <h2>Use of Account</h2>
+    <p>
+      Each purchase of a paid {ECompanyName.ABBREVIATION} account (“Subscription”) is licensed for use
+      by one company or organization only. If you would like to use a Subscriptions account across multiple
+      locations, you can do so but sharing accounts with other individuals or organizations outside the
+      company or organization is strictly prohibited. You may not authorize others to use your account,
+      and you may not assign or otherwise transfer your account to any other person or entity unless
+      otherwise stated in writing by {ECompanyName.ABBREVIATION} authorized staff. You are entirely responsible
+      for any and all activities that occur under your account.
+    </p>
+    <h2>Uploaded Videos, Graphics & Images</h2>
+    <p>We do not claim any rights to videos, images or text that you upload.</p>
+    <h2>Content</h2>
+    <p>
+      We respect the intellectual property rights of others, and require that those people who use {ECompanyName.ABBREVIATION},
+      or the services or features made available on or through the site, do the same. End Users are
+      responsible for any Content submitted by the End User. You agree to the following:
+    </p>
+    <ul>
+      <li>you own the intellectual property rights in that Product;</li>
+      <li>that Product does not infringe the intellectual property rights of a third party;</li>
+      <li>that Product is not false, inaccurate or misleading;</li>
+      <li>that Product is not a stolen or an unlawful item;</li>
+      <li>
+        that Product does not violate any applicable law, statute, ordinance or regulation
+        (including but not limited to, those governing export control, consumer protection, unfair
+        competition, criminal law, anti-discrimination or trade practices or fair trading laws);
+      </li>
+      <li>that Product is not defamatory, unlawfully threatening or unlawfully harassing;</li>
+      <li>
+        that Product is not obscene and does not contain child pornography and does not constitute
+        or contain material that is adult in nature or harmful to persons under the age of 18 years;
+      </li>
+      <li>
+        that Product does not contain any content that may be considered as prohibited or
+        potentially prohibited content for the purposes of the Broadcasting Services Act 1992 (Cth);
+      </li>
+      <li>
+        that Product would not cause {ECompanyName.ABBREVIATION} to violate any applicable law, statute,
+        ordinance or regulation by making it available on the Sites.
+      </li>
+    </ul>
+
+    <h2>General Conditions</h2>
+    <p>
+      We reserve the right to refuse service to anyone for any reason at any time. We reserve the
+      right to force forfeiture of any account that becomes inactive, violates trademark, or may
+      mislead other users. We may, but have no obligation to, remove Content and accounts containing
+      Content that we determine in our sole discretion are unlawful, offensive, threatening,
+      libelous, defamatory, obscene or otherwise objectionable or violates any party's intellectual
+      property or these Terms of Usage.
+    </p>
+    <h2>Property Rights</h2>
+    <p>
+      {ECompanyName.ABBREVIATION} does NOT claim ANY ownership rights in the text, files, fonts, images,
+      photos, video, sounds, musical works, works of authorship, applications, or any other materials
+      (collectively, "Content") that you upload on or through the {ECompanyName.ABBREVIATION} Sites or
+      Applications.
+    </p>
+    <p>
+      {ECompanyName.ABBREVIATION} does NOT claim ANY ownership or license to the content you create using
+      {ECompanyName.ABBREVIATION}. By using {ECompanyName.ABBREVIATION} you agree that you have explicit
+      permission to use any graphics, logos, fonts, photos or other content you upload. You indemnify
+      {ECompanyName.ABBREVIATION}, its contractors, and its licensors, and their respective
+      directors, officers, employees and agents from and against any and all claims and expenses,
+      including attorneys' fees, arising out of your use of the Site or the Service, including but
+      not limited to out of your violation of this Agreement. You represent and warrant that: (i)
+      you own the Content uploaded by you on or through the {ECompanyName.ABBREVIATION} Site and Applications,
+      (ii) the uploading and use of your Content on or through the {ECompanyName.ABBREVIATION} Sites
+      and Applications does not violate the privacy rights, publicity rights, copyrights, contract rights,
+      intellectual property rights or any other rights of any person, and; (iii) the uploading of your
+      Content on the Sites and Applications do not result in a breach of contract between you and a third
+      party. You agree to pay for all royalties, fees, and any other monies owing any person by reason
+      of Content you upload on or through the Sites and Applications.
+    </p>
+    <h2>Governing Law</h2>
+    <p>
+      This Agreement (and any further rules, policies, or guidelines incorporated by reference)
+      shall be governed and construed in accordance with the laws of the State of Texas, without
+      giving effect to any principles of conflicts of law.
+    </p>
+    <h2>Changes To This Agreement</h2>
+    <p>
+      We reserve the right, at our sole discretion, to modify or replace these Terms of Usage by
+      posting the updated terms on the Sites and Applications. Your continued use of the Site after
+      any such changes constitutes your acceptance of the new Terms of Usage. Please review this
+      Agreement periodically for changes. If you do not agree to any of this Agreement or any
+      changes to this Agreement, do not use, access or continue to access the Site or discontinue
+      any use of the Site immediately.
+    </p>
+  </div>
+</div>
---
+++ src/routes/terms/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Footer from '$components/footer/Footer.svelte';
+  import Header from '$components/header/Header.svelte';
+  import Terms from '$components/terms/Terms.svelte';
+  import { EToken } from '$l18n/enums';
+  import { t } from '$l18n';
+</script>
+
+<svelte:head>
+  <title>Arky - {$t(EToken.TERMS_SHORT)}</title>
+</svelte:head>
+
+<div class="flex flex-col">
+  <Header isLegalPage />
+  <Terms />
+</div>
+
+<Footer />
