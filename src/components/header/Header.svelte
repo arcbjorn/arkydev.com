6f08e8c5b2d72e3405487cc7b555d426e43af271
arcbjorn <arky.happ@gmail.com>
2022-09-30 02:18:39 +0400

    feat(Header): add legal pages logic

--- src/components/header/Header.svelte
+++ src/components/header/Header.svelte
@@ -3,23 +3,47 @@
   import { EToken } from '$l18n/enums';
   import Navigation from '$components/header/Navigation.svelte';
   import { ECompanyName } from '$components/enums';
+
+  export let isLegalPage: boolean = false;
 </script>
 
 <header>
-  <div class="onlyLargeScreen" />
-  <div class="onlyLargeScreen">
-    <Navigation />
-  </div>
+  {#if isLegalPage}
+    <a href="/" class="flex flex-col">
+      <span class="text-md font-bold">{ECompanyName.FULL}</span>
+      <span class="text-xs">{$t(EToken.SLOGAN)}</span>
+    </a>
+  {:else}
+    <a href="/" class="hidden md:flex lg:hidden flex-col">
+      <span class="text-md font-bold">{ECompanyName.FULL}</span>
+      <span class="text-xs">{$t(EToken.SLOGAN)}</span>
+    </a>
 
-  <a href="/" class="hidden md:flex lg:hidden flex-col">
-    <span class="text-md font-bold">{ECompanyName.FULL}</span>
-    <span class="text-xs">{$t(EToken.SLOGAN)}</span>
-  </a>
+    <a href="/" class="md:hidden text-lg font-bold">{ECompanyName.SHORT}</a>
+  {/if}
 
-  <a href="/" class="md:hidden text-lg font-bold">{ECompanyName.SHORT}</a>
+  {#if isLegalPage}
+    <div />
+  {:else}
+    <div class="onlyLargeScreen" />
+    <div class="onlyLargeScreen">
+      <Navigation />
+    </div>
 
-  <div class="invisible lg:hidden">MobileNavigation</div>
+    <div class="invisible lg:hidden">MobileNavigation</div>
+  {/if}
 </header>
+
+{#if isLegalPage}
+  <a href="/">
+    <button
+      type="button"
+      title="Back"
+      class="fixed z-90 bottom-10 left-8 flex justify-center items-center border-2 border-black bg-white w-16 h-16 drop-shadow-lg hover:text-white text-5xl hover:bg-black"
+      >&#8592;</button
+    >
+  </a>
+{/if}
 
 <style>
   .onlyLargeScreen {
