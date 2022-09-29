f994c6f3ce8e88b1077b0a463cee399c00ed59e1
arcbjorn <arky.happ@gmail.com>
2022-09-30 02:10:29 +0400

    feat(components): add email enum

--- src/components/enums.ts
+++ src/components/enums.ts
@@ -1,5 +1,11 @@
 export enum ECompanyName {
+  ABBREVIATION = 'ASDC',
   SHORT = 'Arky',
   FULL = 'Arky Development',
   EXTENDED = 'Arky Software Development Corporation'
+}
+
+export enum ECompanyEmail {
+  HELLO = 'hi@arkydev.com',
+  PRIVACY = 'privacy@arkydev.com'
 }
