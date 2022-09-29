94b120bfec7c179e90837814cacd8f71901971a6
arcbjorn <arky.happ@gmail.com>
2022-09-30 02:14:52 +0400

    feat(legal): add Legal mentions page

---
+++ src/components/legal/Legal.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { ECompanyName } from '$components/enums';
+</script>
+
+<main>
+  <div class="flex p-4 m-8 sm:m-0 md:p-8 border-2 bg-white border-black max-w-fit">
+    Oleg Luganskiy<br />
+    DBA. "{ECompanyName.EXTENDED}"<br />
+    &nbsp;<br />
+    Registered ID - 316366767<br />
+    &nbsp;<br />
+    Legal Address:<br />
+    19 Shartava Avenue<br />
+    3700 Rustavi<br />
+    Georgia
+  </div>
+</main>
+
+<style>
+  main {
+    @apply grow w-full flex flex-col justify-center items-center border-b-2 bg-white;
+    background: url(/main_bg.svg) no-repeat center;
+  }
+</style>
---
+++ src/routes/legal/+page.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Footer from '$components/footer/Footer.svelte';
+  import Header from '$components/header/Header.svelte';
+  import Legal from '$components/legal/Legal.svelte';
+  import { EToken } from '$l18n/enums';
+  import { t } from '$l18n';
+</script>
+
+<svelte:head>
+  <title>Arky - {$t(EToken.LEGAL_SHORT)}</title>
+</svelte:head>
+
+<div class="flex flex-col h-full">
+  <Header isLegalPage />
+  <Legal />
+</div>
+
+<Footer />
