e3ab7f86541055e63c8cf3fd6752d6558deab8f3
arcbjorn <arky.happ@gmail.com>
2022-09-30 12:50:16 +0400

    ref(header): move navOptions to file

--- src/components/header/Navigation.svelte
+++ src/components/header/Navigation.svelte
@@ -1,23 +1,6 @@
 <script lang="ts">
   import { t } from '$l18n';
-
-  import { NAVIGATION_ANCHOR, type INavigationOption } from '$routes/types';
-  import { EToken } from '$l18n/enums';
-
-  const navOptions: INavigationOption[] = [
-    {
-      token: EToken.SERVICES,
-      anchor: NAVIGATION_ANCHOR[EToken.SERVICES]
-    },
-    {
-      token: EToken.ABOUT,
-      anchor: NAVIGATION_ANCHOR[EToken.ABOUT]
-    },
-    {
-      token: EToken.CONTACT,
-      anchor: NAVIGATION_ANCHOR[EToken.CONTACT]
-    }
-  ];
+  import navOptions from '$components/header/navOptions';
 </script>
 
 <nav class="flex justify-around">
