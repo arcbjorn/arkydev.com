1c1dfdaeed82dbd75e0962026a3c894418434532
arcbjorn <arky.happ@gmail.com>
2022-09-30 10:49:45 +0400

    feat(Tailwind): extend media query classes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
+    screens: {
+      '2xs': '325px',
+      xs: '475px',
+      ...defaultTheme.screens
+    },
     extend: {}
   },
   plugins: [require('@tailwindcss/typography')]
