c9684516e687cce7e2b452ea9d995455f98c53b4
arcbjorn <arky.happ@gmail.com>
2025-08-15 03:55:49 -0300

    refactor(header): use extracted CSS classes in Navigation

--- src/components/header/Navigation.svelte
+++ src/components/header/Navigation.svelte
@@ -7,43 +7,10 @@
 
 <nav class="flex justify-around items-center">
   {#each navOptions as option}
-    <a href={option.anchor} class="option">
+    <a href={option.anchor} class="nav-option">
       {$t(option.token)}
     </a>
   {/each}
   <ThemeToggle />
   <LanguageToggle />
 </nav>
-
-<style>
-  .option {
-    @apply px-4 sm:px-8 dark:text-white;
-  }
-
-  .option:hover {
-    position: relative;
-
-    background:
-      linear-gradient(
-          to bottom,
-          currentColor 1.2px,
-          transparent 1.2px,
-          transparent calc(100% - 1.2px),
-          currentColor calc(100% - 1.2px)
-        )
-        no-repeat,
-      linear-gradient(
-          to left,
-          currentColor 1.2px,
-          transparent 1.2px,
-          transparent calc(100% - 1.2px),
-          currentColor calc(100% - 1.2px)
-        )
-        no-repeat;
-
-    background-position: center;
-    background-size:
-      calc(100% - 0.75em) 100%,
-      100% calc(100% - 0.75em);
-  }
-</style>
