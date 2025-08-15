a2060e7eb92ee08d31e3ed595edf26f02ca22a9e
arcbjorn <arky.happ@gmail.com>
2025-08-15 03:55:58 -0300

    refactor(header): modernize MobileNavigation with stores and components

--- src/components/header/MobileNavigation.svelte
+++ src/components/header/MobileNavigation.svelte
@@ -2,59 +2,66 @@
   import { t } from '$l18n';
   import navOptions from '$components/header/navOptions';
   import Logo from '$components/common/Logo.svelte';
+  import Button from '$components/common/Button.svelte';
+  import { isMenuOpen, toggleMenu, closeMenu } from '$stores/navigation';
+  import { onMount } from 'svelte';
 
-  let isOpen = false;
+  onMount(() => {
+    const unsubscribe = isMenuOpen.subscribe((open) => {
+      if (typeof document !== 'undefined') {
+        if (open) {
+          document.body.classList.add('menu-open');
+        } else {
+          document.body.classList.remove('menu-open');
+        }
+      }
+    });
 
-  const toggleMenu = () => (isOpen = !isOpen);
+    return unsubscribe;
+  });
+
+  const handleLinkClick = () => {
+    closeMenu();
+  };
 </script>
 
-<svelte:head>
-  {#if isOpen}
-    <style>
-      body {
-        overflow: hidden;
-      }
-    </style>
-  {/if}
-</svelte:head>
-
 <button
-  class="w-10 h-10 relative focus:outline-none bg-white dark:bg-stone-950 border-2 border-black dark:border-gray-300 hover:bg-black dark:hover:bg-white transition-colors duration-300 z-50 group ml-4"
+  class="mobile-nav-button"
   on:click={toggleMenu}
 >
   <span class="sr-only">Open main menu</span>
 
-  <div class="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+  <div class="hamburger-container">
     <span
-      class="block absolute h-0.5 w-5 bg-black dark:bg-white group-hover:bg-white dark:group-hover:bg-black transform transition duration-500 ease-in-out"
-      class:rotate-45={isOpen}
-      class:-translate-y-1.5={!isOpen}
+      class="hamburger-line"
+      class:rotate-45={$isMenuOpen}
+      class:-translate-y-1.5={!$isMenuOpen}
     />
     <span
-      class="block absolute h-0.5 w-5 bg-black dark:bg-white group-hover:bg-white dark:group-hover:bg-black transform transition duration-500 ease-in-out"
-      class:opacity-0={isOpen}
+      class="hamburger-line"
+      class:opacity-0={$isMenuOpen}
     />
     <span
-      class="block absolute h-0.5 w-5 bg-black dark:bg-white group-hover:bg-white dark:group-hover:bg-black transform transition duration-500 ease-in-out"
-      class:-rotate-45={isOpen}
-      class:translate-y-1.5={!isOpen}
+      class="hamburger-line"
+      class:-rotate-45={$isMenuOpen}
+      class:translate-y-1.5={!$isMenuOpen}
     />
   </div>
 </button>
 
-{#if isOpen}
-  <div class="fixed inset-0 z-40 bg-white dark:bg-stone-950">
-    <div class="flex flex-col items-center justify-center h-screen">
-      <ul class="space-y-8 text-center">
+{#if $isMenuOpen}
+  <div class="mobile-menu-overlay">
+    <div class="mobile-menu-container">
+      <ul class="mobile-menu-list">
         {#each navOptions as option}
           <li>
-            <a
-              class="text-2xl px-8 py-4 inline-block text-black dark:text-white hover:bg-black hover:text-white dark:hover:bg-white dark:hover:text-black transition-colors duration-300"
+            <Button
+              variant="mobile-nav"
               href={option.anchor}
-              on:click={toggleMenu}
+              on:click={handleLinkClick}
             >
               {$t(option.token)}
-            </a>
+            </Button>
           </li>
         {/each}
       </ul>
